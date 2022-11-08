47808eb2c3ad2c6e8ca057ea6857d9e4ed2494ff
Amanda Lee <leeandama87@gmail.com>
2022-11-08 13:35:02 -0800

    Styling in CSS, added images including gifs to add motion to the webpage. Used media queries to make the page responsive/a clean layout. Resized images to fit inside of flexbox containers using object-fit. Added background image behind title.

---
+++ amanda/js/movie.js
@@ -0,0 +1,20 @@
+// Add event listener to image
+var theater = document.getElementById("theaters-img")
+
+theater.addEventListener("click", imageLink);
+
+var links = [
+    youtube = "https://www.youtube.com/results?search_query=movie+trailers+in+theaters+now",
+    rottenTomatoes = "",
+    reddit = "",
+]
+
+function imageLink() {
+    // Creating anchor element for images
+    var anchorEl = document.createElement("a");
+    // Creating button to append link (a)
+    var redirectToLink = document.createElement("button");
+    anchorEl.appendChild(redirectToLink);
+    // Link to separate webpage
+    
+}
