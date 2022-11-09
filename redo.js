b34931fa9ef3176a353365a5cb75b576c8aeeb9f
Floyd Hudson <hudsonfloyd@yahoo.com>
2022-11-09 12:44:27 -0600

    replaced all of the var with let and const and changed them from tasks to titles

--- redo.js
+++ redo.js
@@ -1,109 +1,109 @@
 // On app load, get all tasks from localStorage
-window.onload = loadTasks;
+window.onload = loadTitles;
 
 // On form submit add task
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
-  addTask();
+  addTitle();
 });
 
-function loadTasks() {
+function loadTitles() {
   // check if localStorage has any tasks
   // if not then return
   if (localStorage.getItem("titles") == null) return;
 
   // Get the tasks from localStorage and convert it to an array
-  let tasks = Array.from(JSON.parse(localStorage.getItem("titles")));
+  let titles = Array.from(JSON.parse(localStorage.getItem("titles")));
 
   // Loop through the tasks and add them to the list
-  tasks.forEach(task => {
+  titles.forEach(title => {
     const list = document.querySelector("ul");
     const li = document.createElement("li");
-    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
-      <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-      <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    li.innerHTML = `<input type="checkbox" onclick="titleComplete(this)" class="check" ${title.completed ? 'checked' : ''}>
+      <input type="text" value="${title.title}" class="title ${title.completed ? 'completed' : ''}" onfocus="getCurrentTitle(this)" onblur="editTitle(this)">
+      <i class="fa fa-trash" onclick="removeTitle(this)"></i>`;
     list.insertBefore(li, list.children[0]);
   });
 }
 
-function addTask() {
-  const task = document.querySelector("form input");
+function addTitle() {
+  const title = document.querySelector("form input");
   const list = document.querySelector("ul");
-  // return if task is empty
-  if (task.value === "") {
+  // return if title is empty
+  if (title.value === "") {
     return false;
   }
-  // check is task already exist
-  if (document.querySelector(`input[value="${task.value}"]`)) {
+  // check is title already exist
+  if (document.querySelector(`input[value="${title.value}"]`)) {
     return false;
   }
 
-  // add task to local storage
-  localStorage.setItem("titles", JSON.stringify([...JSON.parse(localStorage.getItem("titles") || "[]"), { task: task.value, completed: false }]));
+  // add title to local storage
+  localStorage.setItem("titles", JSON.stringify([...JSON.parse(localStorage.getItem("titles") || "[]"), { title: title.value, completed: false }]));
 
   // create list item, add innerHTML and append to ul
   const li = document.createElement("li");
-  li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
-  <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-  <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+  li.innerHTML = `<input type="checkbox" onclick="titleComplete(this)" class="check">
+  <input type="text" value="${title.value}" class="title" onfocus="getCurrentTitle(this)" onblur="editTitle(this)">
+  <i class="fa fa-trash" onclick="removeTitle(this)"></i>`;
   list.insertBefore(li, list.children[0]);
   // clear input
-  task.value = "";
+  title.value = "";
 }
 
-function taskComplete(event) {
-  let tasks = Array.from(JSON.parse(localStorage.getItem("titles")));
-  tasks.forEach(task => {
-    if (task.task === event.nextElementSibling.value) {
-      task.completed = !task.completed;
+function titleComplete(event) {
+  let titles = Array.from(JSON.parse(localStorage.getItem("titles")));
+  titles.forEach(title => {
+    if (title.title === event.nextElementSibling.value) {
+      title.completed = !title.completed;
     }
   });
-  localStorage.setItem("titles", JSON.stringify(tasks));
+  localStorage.setItem("titles", JSON.stringify(titles));
   event.nextElementSibling.classList.toggle("completed");
 }
 
-function removeTask(event) {
-  let tasks = Array.from(JSON.parse(localStorage.getItem("titles")));
-  tasks.forEach(task => {
-    if (task.task === event.parentNode.children[1].value) {
-      // delete task
-      tasks.splice(tasks.indexOf(task), 1);
+function removeTitle(event) {
+  let titles = Array.from(JSON.parse(localStorage.getItem("titles")));
+  titles.forEach(title => {
+    if (title.title === event.parentNode.children[1].value) {
+      // delete title
+      titles.splice(titles.indexOf(title), 1);
     }
   });
-  localStorage.setItem("titles", JSON.stringify(tasks));
+  localStorage.setItem("titles", JSON.stringify(titles));
   event.parentElement.remove();
 }
 
 // store current task to track changes
-var currentTask = null;
+var currentTitle = null;
 
 // get current task
-function getCurrentTask(event) {
-  currentTask = event.value;
+function getCurrentTitle(event) {
+  currentTitle = event.value;
 }
 
 // edit the task and update local storage
 function editTask(event) {
-  let tasks = Array.from(JSON.parse(localStorage.getItem("titles")));
+  let titles = Array.from(JSON.parse(localStorage.getItem("titles")));
   // check if task is empty
   if (event.value === "") {
-    event.value = currentTask;
+    event.value = currentTitle;
     return;
   }
   // task already exist
-  tasks.forEach(task => {
-    if (task.task === event.value) {
-      event.value = currentTask;
+  titles.forEach(title => {
+    if (title.title === event.value) {
+      event.value = currentTitle;
       return;
     }
   });
-  // update task
-  tasks.forEach(task => {
-    if (task.task === currentTask) {
-      task.task = event.value;
+  // update title
+  titles.forEach(title => {
+    if (title.title === currentTitle) {
+      title.title = event.value;
     }
   });
   // update local storage
-  localStorage.setItem("titles", JSON.stringify(tasks));
+  localStorage.setItem("titles", JSON.stringify(titles));
 }
   
